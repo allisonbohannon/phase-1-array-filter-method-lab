0f9f40c38dcc7c1d1f07ddf927e9f422dc213d49
Allison Bohannon <akboh.github@gmail.com>
2022-04-18 20:12:21 -0700

    commit tree e3134e35dd60e830494e0d36dbaac6e4189ac9ee

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(arr, str) {
+    return arr.filter(item => item.toLowerCase() === str.toLowerCase());
+}
+
+function fuzzyMatch(arr, str) {
+    return arr.filter(item => item.slice(0, str.length) === str); 
+}
+
+console.log(fuzzyMatch([ 'Sammy', 'Sarah', 'Sally' ], 'S')); 
+
+function matchName(arr, str) {
+    return arr.filter(item => item.name === str); 
+}; 
+
